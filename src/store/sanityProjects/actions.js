0fe2699e5a4be0914457f313f34328c739d21437
NunoCPNP <nunocpereira@outlook.com>
2019-10-28 10:27:41 +0000

    projects fetch

--- src/store/sanityProjects/actions.js
+++ src/store/sanityProjects/actions.js
@@ -1,3 +1,4 @@
+/* eslint no-undef: 0 */
 const API_URL = process.env.REACT_APP_API_URL
 const API_QUERY = `*[ _type == "projects" ]{
                     "id" : _id,
@@ -10,16 +11,16 @@ const API_QUERY = `*[ _type == "projects" ]{
 
 const TRSLT_QUERY = encodeURIComponent(API_QUERY)
 
-export const GET_DATA = 'GET_DATA'
+export const GET_PROJECTS = 'GET_PROJECTS'
 
-export function getData () {
+export function getProjects () {
   return async function (dispatch) {
     const res = await fetch(
       `${API_URL}v1/data/query/production?query=${TRSLT_QUERY}`
     )
     const data = await res.json()
     return dispatch({
-      type: 'GET_DATA',
+      type: 'GET_PROJECTS',
       data: data.result
     })
   }
