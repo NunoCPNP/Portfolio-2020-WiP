aa8204d8810e3e86647e965f66e97320eab8ace1
Nuno Pereira <nuno.pereira@itsector.pt>
2020-04-15 18:51:17 +0100

    pic

--- src/dev-data/projects.js
+++ src/dev-data/projects.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     id: '0000001',
     title: 'Plume Support',
-    cover: 'https://andrewhawkes.github.io/codepen-assets/steam-game-cards/game_1.jpg',
+    cover: 'https://i.postimg.cc/QNQ5Jyp2/project-1.jpg',
     description: 'Plume Support website built with ...',
     usedTechnology: ['ZenDesk', 'HTML', 'SCSS', 'JavaScript'],
     publicCode: false,
@@ -13,7 +13,7 @@ export const projects = [
   {
     id: '0000002',
     title: 'Plume Support',
-    cover: 'https://andrewhawkes.github.io/codepen-assets/steam-game-cards/game_2.jpg',
+    cover: 'https://i.postimg.cc/7Yqzsn9f/project-2.jpg',
     description: 'Plume Support website built with ...',
     usedTechnology: ['ZenDesk', 'HTML', 'SCSS', 'JavaScript'],
     publicCode: false,
