92587846794eeaa01756cc1c97619b46296afd31
Nuno Pereira <nuno.pereira@itsector.pt>
2020-05-13 17:57:58 +0100

    better tests

--- src/components/aboutTextBock/AboutTextBlock.test.tsx
+++ src/components/aboutTextBock/AboutTextBlock.test.tsx
@@ -4,21 +4,19 @@ import { shallow } from 'enzyme'
 import AboutTextBlock from './AboutTextBlock'
 import { Wrapper } from './AboutTextBlock.styles'
 
-const defaultProps = {}
-
 const setup = (props = {}) => {
-  const setupProps = { ...defaultProps, ...props }
-  return shallow(<AboutTextBlock {...setupProps} />)
+  return shallow(<AboutTextBlock {...props} />)
 }
 
 describe('AboutTextBlock Component Tests: ', () => {
+  let wrapper: any
+  beforeEach(() => (wrapper = setup()))
+
   it('Renders without errors', () => {
-    const wrapper = setup()
     expect(wrapper.type()).toEqual(Wrapper)
   })
 
   it('Have 3 paragraph tags', () => {
-    const wrapper = setup()
     expect(wrapper.find('p').length).toBe(3)
   })
 })
--- src/components/blogBar/BlogBar.test.tsx
+++ src/components/blogBar/BlogBar.test.tsx
@@ -8,53 +8,60 @@ import reducer from '../../store/controllers/reducers'
 import BlogBar from './BlogBar'
 import { BarWrapper, Button, StyledP } from './BlogBar.styles'
 
-const defaultProps = {}
-
 const setup = (props = {}, state: any = {}) => {
-  const setupProps = { ...defaultProps, ...props }
   const mockStore = createStore(reducer, state)
 
   return mount(
     <Provider store={mockStore}>
-      <BlogBar {...setupProps} />
+      <BlogBar {...props} />
     </Provider>,
   )
 }
 
 describe('BlogBar Component Tests:', () => {
-  it('Renders without errors', () => {
-    const wrapper = setup({}, { users: { currentUser: null } })
-    expect(wrapper.find(BarWrapper).length).toBe(1)
+  describe('No user logged in', () => {
+    let wrapper: any
+
+    beforeEach(() => {
+      wrapper = setup({}, { users: { currentUser: null } })
+    })
+
+    it('Renders without errors', () => {
+      expect(wrapper.find(BarWrapper).length).toBe(1)
+    })
+
+    it('If user is null shows login', () => {
+      const target = wrapper.find(Button)
+      expect(target.length).toBe(1)
+      expect(target.text()).toBe('Login')
+    })
+
+    it('If user is null will not show logout', () => {
+      const target = wrapper.find(StyledP)
+      expect(target.length).toBe(0)
+    })
   })
 
-  it('If user is null shows login', () => {
-    const wrapper = setup({}, { users: { currentUser: null } })
-    const target = wrapper.find(Button)
-    expect(target.length).toBe(1)
-    expect(target.text()).toBe('Login')
-  })
+  describe('User logged in', () => {
+    let wrapper: any
 
-  it('If user is null will not show logout', () => {
-    const wrapper = setup({}, { users: { currentUser: null } })
-    const target = wrapper.find(StyledP)
-    expect(target.length).toBe(0)
-  })
+    beforeEach(() => {
+      wrapper = setup({}, { users: { currentUser: { displayName: 'test user' } } })
+    })
 
-  it('If user is defined shows logout', () => {
-    const wrapper = setup({}, { users: { currentUser: { displayName: 'test user' } } })
-    const target = wrapper.find(StyledP)
-    expect(target.length).toBe(1)
-  })
+    it('If user is defined shows logout', () => {
+      const target = wrapper.find(StyledP)
+      expect(target.length).toBe(1)
+    })
 
-  it('If user is defined will not return login', () => {
-    const wrapper = setup({}, { users: { currentUser: { displayName: 'test user' } } })
-    const target = wrapper.find(Button)
-    expect(target.text()).not.toBe('Login')
-  })
+    it('If user is defined will not return login', () => {
+      const target = wrapper.find(Button)
+      expect(target.text()).not.toBe('Login')
+    })
 
-  it('If user is defined will display is name', () => {
-    const wrapper = setup({}, { users: { currentUser: { displayName: 'test user' } } })
-    const target = wrapper.find(StyledP).find('span')
-    expect(target.text()).toBe('test user')
+    it('If user is defined will display is name', () => {
+      const target = wrapper.find(StyledP).find('span')
+      expect(target.text()).toBe('test user')
+    })
   })
 })
